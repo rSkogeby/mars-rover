bb3b9e10e521587f3a8360fc287c0e44b219fe92
Richard Skogeby <richard.skogeby@outlook.com>
2020-03-10 21:02:51 +0000

    Solve CORS issues

--- server/views/live_rover_map.py
+++ server/views/live_rover_map.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://0.0.0.0:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 
 class Item(BaseModel):
     Input: str
