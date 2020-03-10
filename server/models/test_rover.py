963e9a5e13a3c88da49efb63ddcbff7d39ff4ff9
Richard Skogeby <richard.skogeby@outlook.com>
2020-03-10 21:42:14 +0000

    Extend left turn test and add right turn test

--- server/models/test_rover.py
+++ server/models/test_rover.py
@@ -12,6 +12,25 @@ class TestRover(unittest.TestCase):
         self.rover.d = 'N'
         self.deploy_rover._turn_left(self.rover)
         self.assertEqual(self.rover.d, 'W')
+        self.deploy_rover._turn_left(self.rover)
+        self.assertEqual(self.rover.d, 'S')
+        self.deploy_rover._turn_left(self.rover)
+        self.assertEqual(self.rover.d, 'E')
+        self.deploy_rover._turn_left(self.rover)
+        self.assertEqual(self.rover.d, 'N')
+
+    def test_right_turn(self):
+        self.rover.d = 'N'
+        self.deploy_rover._turn_right(self.rover)
+        self.assertEqual(self.rover.d, 'E')
+        self.deploy_rover._turn_right(self.rover)
+        self.assertEqual(self.rover.d, 'S')
+        self.deploy_rover._turn_right(self.rover)
+        self.assertEqual(self.rover.d, 'W')
+        self.deploy_rover._turn_right(self.rover)
+        self.assertEqual(self.rover.d, 'N')
+
+
 
 if __name__ == "__main__":
     unittest.main()
